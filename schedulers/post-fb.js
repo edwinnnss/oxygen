7b29f75416c86b77602208d45a543fa41bbe2f94
dwncy <sw-dev1@formativeai.com>
2019-07-12 17:31:03 +0800

    push

---
+++ schedulers/post-fb.js
@@ -0,0 +1,58 @@
+require('../src/init');
+
+const _ = require('lodash');
+const fs = require('fs');
+const request = require('request-promise');
+const Bluebird = require('bluebird');
+const Movie = require('../src/database/models/movie');
+const { QUALITY_FORMAT_MAPPING } = require('../src/constants');
+
+const getRandomMovie = async () => {
+  const totalMovie = await Movie.count();
+
+  const random = Math.floor(Math.random() * totalMovie);
+
+  let movie = await Movie.findOne().skip(random);
+
+  const slugsHasBeenPosted = JSON.parse(fs.readFileSync('schedulers/slug.json'));
+
+  if (slugsHasBeenPosted.find(slug => slug === movie.slug)) {
+    movie = await getRandomMovie();
+  } else {
+    fs.writeFileSync(
+      'schedulers/slug.json',
+      JSON.stringify(slugsHasBeenPosted.concat(movie.slug), null, 2),
+    );
+  }
+
+  return movie;
+};
+
+Bluebird.resolve()
+  .then(async () => {
+    console.log('Getting random movie...');
+    const movie = await getRandomMovie();
+
+    const message = `Nonton film ${movie.name}`
+      + `\nQuality: ${_.get(QUALITY_FORMAT_MAPPING, `${movie.quality}.name`, 'HD')}`
+      + ` | Genre: ${_.join(_.map(movie.genres, 'label'), ', ')}`
+      + `\n\n${movie.summary.length > 150 ? movie.summary.substring(0, 147) + '...' : movie.summary}`
+      + `\n\nlink: https://nontongue.xyz/movie/${movie.slug}`;
+
+    const url = `https://graph.facebook.com/v3.3/${process.env.PAGE_ID}/feed`
+      + `?access_token=${process.env.PAGE_ACCESS_TOKEN}`
+      + `&message=${message}`
+      + `&link=https://nontongue.xyz/movie/${movie.slug}`;
+
+    console.log(`Post url: ${url}`);
+
+    const result = await request.post(url);
+
+    if (!_.isEmpty(result)) {
+      console.log(`Successfully posting to facebook with slug: ${movie.slug}`);
+    } else {
+      console.log(`Failed posting to facebook with slug: ${movie.slug}`);
+    }
+  })
+  .catch(console.log)
+  .finally(() => process.exit(3));
--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,33 @@ exports.HOUR = 60 * exports.MINUTE;
 exports.DAY = 24 * exports.HOUR;
 
 exports.MAX_RECOMMENDATION_ITEM = 6;
+
+exports.QUALITY_FORMAT_MAPPING = {
+  'HD': {
+    name: 'HD',
+  },
+  'HD (Low)': {
+    name: 'HD',
+  },
+  'FULL HD': {
+    name: 'HD',
+  },
+  'Blu-ray': {
+    name: 'HD',
+  },
+  'CAM': {
+    name: 'CAM',
+  },
+  'SD': {
+    name: 'SD',
+  },
+  'HD (High)': {
+    name: 'HD',
+  },
+  'FULL HD (Low)': {
+    name: 'HD',
+  },
+  '4K HD': {
+    name: '4K',
+  },
+};
