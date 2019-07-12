85419bbfb04e220bdb5532963a76c825cf2f43f8
dwncy <sw-dev1@formativeai.com>
2019-07-12 23:18:19 +0800

    fix dotenv

--- src/init.js
+++ src/init.js
@@ -1,10 +1,11 @@
 /**
  * Init dotenv and init database model
  */
-
-require('dotenv').load();
 const mongoose = require('mongoose');
 const Bluebird = require('bluebird');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: __dirname + '/../.env' });
 
 mongoose.Promise = Bluebird;
 mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING, { useNewUrlParser: true })
